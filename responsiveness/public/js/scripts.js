e32f3c85a3a8480a02d85f83fa44678d4de3c3ea
daniel arbiol <arbiol.daniel0727@gmail.com>
2014-04-25 13:42:33 +0800

    finished multi window responsive test

--- responsiveness/public/js/scripts.js
+++ responsiveness/public/js/scripts.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+$( document ).ready( function(){
 	console.log('doc is ready');
-})
+
+	$( '#goto-website' ).click( function (){
+		var website = $(this).prev().val();
+		console.log(website);
+		$('main').find('.iframe').each( function(){
+			console.log($(this).attr('src', website));
+		} );
+	} );
+
+	$( '#refresh-website' ).click( function(){
+		window.location.reload();
+	} );
+} )
