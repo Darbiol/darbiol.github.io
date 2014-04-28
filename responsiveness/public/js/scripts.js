884fd4000d9390708964314eced1bc516fa815ff
daniel arbiol <arbiol.daniel0727@gmail.com>
2014-04-29 07:45:34 +0800

    disabled custom viewport size

--- responsiveness/public/js/scripts.js
+++ responsiveness/public/js/scripts.js
@@ -32,6 +32,7 @@ $( document ).ready( function(){
 		});
 		//console.log(resolution);
 	} );
+
 // =========================  Contents Behavior ====================
 
 	$( 'li' ).click( function(){
